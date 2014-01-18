98e6128379990c0656b2d927d5e99aaa62222795
Martin Schrodt <martin@schrodt.org>
2014-01-18 14:47:05 +0100

    make array decoder return an array instead of a list

--- src/main/java/clarity/decoder/prop/ArrayDecoder.java
+++ src/main/java/clarity/decoder/prop/ArrayDecoder.java
@@ -1,23 +1,20 @@
 package clarity.decoder.prop;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import clarity.decoder.EntityBitStream;
 import clarity.decoder.Util;
 import clarity.model.Prop;
 
-public class ArrayDecoder implements PropDecoder<List<Object>> {
+public class ArrayDecoder implements PropDecoder<Object[]> {
 
     @Override
-    public List<Object> decode(EntityBitStream stream, Prop prop) {
+    public Object[] decode(EntityBitStream stream, Prop prop) {
         int count = stream.readNumericBits(Util.calcBitsNeededFor(prop.getNumElements() - 1));
-        List<Object> result = new ArrayList<Object>(count);
+        Object[] result = new Object[count];
         PropDecoder<?> decoder = prop.getTemplate().getType().getDecoder();
-        while (count > 0) {
-            count--;
-            Object x = decoder.decode(stream, prop.getTemplate());
-            result.add(x);
+        int i = 0;
+        while (i < count) {
+            result[i] = decoder.decode(stream, prop.getTemplate());
+            i++;
         }
         return result;
     }
