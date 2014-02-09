305c5832ee132544930b980df33d7b75f7aaa678
Martin Schrodt <martin@schrodt.org>
2014-02-09 14:44:35 +0100

    quick "fix" for replays encoding int64 against tickcount X(

--- src/main/java/clarity/decoder/prop/Int64Decoder.java
+++ src/main/java/clarity/decoder/prop/Int64Decoder.java
@@ -8,6 +8,20 @@ public class Int64Decoder implements PropDecoder<Long> {
 
     @Override
     public Long decode(EntityBitStream stream, Prop prop) {
+        boolean isUnsigned = prop.isFlagSet(PropFlag.UNSIGNED);
+        if (prop.isFlagSet(PropFlag.ENCODED_AGAINST_TICKCOUNT)) {
+            // this integer is encoded against tick count (?)...
+            // in this case, we read a protobuf-style varint
+            long v = stream.readVarInt();
+            if (isUnsigned) {
+                return v; // as is -- why?
+            }
+
+            // ostensibly, this is the "decoding" part in signed cases
+            //System.out.println("encoded against tickcount is " + prop + ": " + v);
+            return (-(v & 1L)) ^ (v >>> 1L);
+        }
+
         boolean negate = false;
         int remainder = prop.getNumBits() - 32;
         if (!prop.isFlagSet(PropFlag.UNSIGNED)) {
