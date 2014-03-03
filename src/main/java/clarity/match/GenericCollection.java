61b36afae6b841109835d942f23f3d0c6fa9bf44
Martin Schrodt <martin@schrodt.org>
2014-03-03 01:48:42 +0100

    make GenericCollection iterable

--- src/main/java/clarity/match/GenericCollection.java
+++ src/main/java/clarity/match/GenericCollection.java
@@ -2,11 +2,12 @@ package clarity.match;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import com.rits.cloning.Cloner;
 
-public class GenericCollection<T> implements Cloneable {
+public class GenericCollection<T> implements Iterable<T>, Cloneable {
     
     private static final Cloner CLONER = new Cloner();
     
@@ -27,6 +28,11 @@ public class GenericCollection<T> implements Cloneable {
     @Override
     public Object clone() {
        return CLONER.deepClone(this);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return values.iterator();
     }
     
 }
