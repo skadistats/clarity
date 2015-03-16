30b0befd52409699db9c0dae8406559643d69a9c
Martin Schrodt <martin@schrodt.org>
2015-03-16 17:20:32 +0100

    big speed improvement

--- src/main/java/skadistats/clarity/decoder/BitStream.java
+++ src/main/java/skadistats/clarity/decoder/BitStream.java
@@ -2,51 +2,63 @@ package skadistats.clarity.decoder;
 
 public class BitStream {
 
-    final int[] words;
+    private final long[] masks = {
+        0x0L,             0x1L,              0x3L,              0x7L,
+        0xfL,             0x1fL,             0x3fL,             0x7fL,
+        0xffL,            0x1ffL,            0x3ffL,            0x7ffL,
+        0xfffL,           0x1fffL,           0x3fffL,           0x7fffL,
+        0xffffL,          0x1ffffL,          0x3ffffL,          0x7ffffL,
+        0xfffffL,         0x1fffffL,         0x3fffffL,         0x7fffffL,
+        0xffffffL,        0x1ffffffL,        0x3ffffffL,        0x7ffffffL,
+        0xfffffffL,       0x1fffffffL,       0x3fffffffL,       0x7fffffffL,
+        0xffffffffL,      0x1ffffffffL,      0x3ffffffffL,      0x7ffffffffL,
+        0xfffffffffL,     0x1fffffffffL,     0x3fffffffffL,     0x7fffffffffL,
+        0xffffffffffL,    0x1ffffffffffL,    0x3ffffffffffL,    0x7ffffffffffL,
+        0xfffffffffffL,   0x1fffffffffffL,   0x3fffffffffffL,   0x7fffffffffffL,
+        0xffffffffffffL,  0x1ffffffffffffL,  0x3ffffffffffffL,  0x7ffffffffffffL,
+        0xfffffffffffffL, 0x1fffffffffffffL, 0x3fffffffffffffL, 0x7fffffffffffffL
+    };
+
+    final long[] data;
     int pos;
 
-    public BitStream(byte[] data) {
+    public BitStream(byte[] bytes) {
 
-        // it seems valve started not sending trailing zero bits in a stream under certain circumstances?
-        // example match id 714979634
-        // append a zero word at the end, which seems to fix it for now
-        this.words = new int[(data.length + 7) / 4];
-        this.pos = 0;
+        data = new long[(bytes.length + 7) / 8];
+        pos = 0;
 
-        int akku = 0;
-        for (int i = 0; i < data.length; i++) {
-            int shift = 8 * (i & 3);
-            int val = ((int) data[i]) & 0xFF;
+        long akku = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            int shift = 8 * (i & 7);
+            long val = ((long) bytes[i]) & 0xFF;
             akku = akku | (val << shift);
-            if ((i & 3) == 3) {
-                words[i / 4] = akku;
+            if ((i & 7) == 7) {
+                data[i / 8] = akku;
                 akku = 0;
             }
         }
-        if ((data.length & 3) != 0) {
-            words[data.length / 4] = akku;
+        if ((bytes.length & 7) != 0) {
+            data[bytes.length / 8] = akku;
         }
-        words[words.length - 1] = 0;
     }
 
-    public int peekNumericBits(int num) {
-        int l = words[pos >> 5];
-        int r = words[(pos + num - 1) >> 5];
-        int shift = pos & 31;
-        int rebuild = (r << (32 - shift)) | (l >>> shift);
-        return (rebuild & ((int)((long)1 << num) - 1));        
-    }
+    public int readNumericBits(int n) {
+        int start = pos >> 6;
+        int end = (pos + n - 1) >> 6;
+        int s = pos & 63;
+        long ret;
 
-    public int readNumericBits(int num) {
-        int result = peekNumericBits(num);
-        pos += num;
-        return result;
+        if (start == end) {
+            ret = (data[start] >>> s) & masks[n];
+        } else { // wrap around
+            ret = ((data[start] >>> s) | (data[end] << (64 - s))) & masks[n];
+        }
+        pos += n;
+        return (int) ret;
     }
 
     public boolean readBit() {
-        boolean result = peekNumericBits(1) != 0;
-        pos += 1;
-        return result;
+        return readNumericBits(1) == 1;
     }
 
     public byte[] readBits(int num) {
@@ -94,10 +106,10 @@ public class BitStream {
     public String toString() {
         StringBuffer buf = new StringBuffer();
 
-        int min = Math.max(0, (pos - 32) / 32);
-        int max = Math.min(words.length - 1, (pos + 63) / 32);
+        int min = Math.max(0, (pos - 32) / 64);
+        int max = Math.min(data.length - 1, (pos + 63) / 64);
         for (int i = min; i <= max; i++) {
-            buf.append(new StringBuffer(String.format("%32s", Integer.toBinaryString(words[i])).replace(' ', '0')).reverse());
+            buf.append(new StringBuffer(String.format("%64s", Long.toBinaryString(data[i])).replace(' ', '0')).reverse());
         }
         buf.insert(pos - min * 32, '*');
         return buf.toString();
