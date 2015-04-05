bae8104033ced1ad30f19e1211e5ecd5ea695934
Martin Schrodt <martin@schrodt.org>
2015-04-05 09:43:29 +0200

    more efficient way to read CDemoFileInfo

--- src/main/java/skadistats/clarity/Clarity.java
+++ src/main/java/skadistats/clarity/Clarity.java
@@ -1,21 +1,38 @@
 package skadistats.clarity;
 
-import skadistats.clarity.processor.runner.FileInfoReader;
+import com.google.protobuf.ZeroCopy;
+import org.xerial.snappy.Snappy;
 import skadistats.clarity.source.InputStreamSource;
+import skadistats.clarity.source.MappedFileSource;
+import skadistats.clarity.source.Source;
+import skadistats.clarity.wire.PacketTypes;
 import skadistats.clarity.wire.proto.Demo;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Clarity {
 
     public static Demo.CDemoFileInfo infoForFile(String fileName) throws IOException {
-    	return infoForStream(new FileInputStream(fileName));
+    	return infoForSource(new MappedFileSource(fileName));
     }
 
     public static Demo.CDemoFileInfo infoForStream(final InputStream stream) throws IOException {
-        return new FileInfoReader(new InputStreamSource(stream)).getFileInfo();
+        return infoForSource(new InputStreamSource(stream));
     }
+
+    public static Demo.CDemoFileInfo infoForSource(final Source src) throws IOException {
+        src.ensureDemoHeader();
+        src.setPosition(src.readFixedInt32());
+        boolean isCompressed = (src.readVarInt32() & Demo.EDemoCommands.DEM_IsCompressed_VALUE) == Demo.EDemoCommands.DEM_IsCompressed_VALUE;
+        src.readVarInt32();
+        int size = src.readVarInt32();
+        byte[] data = src.readBytes(size);
+        if (isCompressed) {
+            data = Snappy.uncompress(data);
+        }
+        return PacketTypes.parse(Demo.CDemoFileInfo.class, ZeroCopy.wrap(data));
+    }
+
 
 }
