3d523799dbd6e7450cc5fe5fa9be2b591310491c
Martin Schrodt <martin@schrodt.org>
2015-08-16 20:08:04 +0200

    sync

---
+++ src/main/java/skadistats/clarity/decoder/s2/prop/UBitVarDecoder.java
@@ -0,0 +1,13 @@
+package skadistats.clarity.decoder.s2.prop;
+
+import skadistats.clarity.decoder.BitStream;
+import skadistats.clarity.decoder.s2.FieldDecoder;
+import skadistats.clarity.model.s2.Field;
+
+public class UBitVarDecoder implements FieldDecoder<Integer> {
+
+    @Override
+    public Integer decode(BitStream bs, Field f) {
+        return bs.readUBitVar();
+    }
+}
