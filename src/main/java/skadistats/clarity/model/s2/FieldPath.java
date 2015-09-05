5c3f6a40250fd3d754befb5d2b15a2dc372f31c5
Martin Schrodt <martin@schrodt.org>
2015-09-05 14:13:45 +0200

    equals & hashcode

--- src/main/java/skadistats/clarity/model/s2/FieldPath.java
+++ src/main/java/skadistats/clarity/model/s2/FieldPath.java
@@ -29,4 +29,28 @@ public class FieldPath {
         }
         return sb.toString();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FieldPath fieldPath = (FieldPath) o;
+        if (last != fieldPath.last) return false;
+        for (int i = 0; i < last; i++) {
+            if (path[i] != fieldPath.path[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        for (int i = 0; i < last; i++) {
+            result = 31 * result + path[i];
+        }
+        return result;
+    }
+
 }
