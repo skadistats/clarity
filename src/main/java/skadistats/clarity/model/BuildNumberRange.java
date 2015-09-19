ffba658935b52f99a91f7324ed8f46076e46058a
Martin Schrodt <martin@schrodt.org>
2015-09-19 15:13:48 +0200

    make Build-Range(null, null) apply even when the build number is not known

--- src/main/java/skadistats/clarity/model/BuildNumberRange.java
+++ src/main/java/skadistats/clarity/model/BuildNumberRange.java
@@ -10,6 +10,7 @@ public class BuildNumberRange {
     }
 
     public boolean appliesTo(int buildNumber) {
+        if (start == null && end == null) return true;
         return buildNumber != -1 && (start == null || start <= buildNumber) && (end == null || end >= buildNumber);
     }
 }
