95e933b36cfcd0c520d3d0bebd2d2d57d4a2b8e3
Martin Schrodt <martin@schrodt.org>
2015-09-21 19:41:07 +0200

    fix hashcode and equals for fieldpaths

--- src/main/java/skadistats/clarity/model/FieldPath.java
+++ src/main/java/skadistats/clarity/model/FieldPath.java
@@ -42,7 +42,7 @@ public class FieldPath {
         if (o == null || getClass() != o.getClass()) return false;
         FieldPath fieldPath = (FieldPath) o;
         if (last != fieldPath.last) return false;
-        for (int i = 0; i < last; i++) {
+        for (int i = 0; i <= last; i++) {
             if (path[i] != fieldPath.path[i]) {
                 return false;
             }
@@ -53,7 +53,7 @@ public class FieldPath {
     @Override
     public int hashCode() {
         int result = 1;
-        for (int i = 0; i < last; i++) {
+        for (int i = 0; i <= last; i++) {
             result = 31 * result + path[i];
         }
         return result;
