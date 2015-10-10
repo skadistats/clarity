49e9e52b2e9360a0b363a6a23c44ff8bbd0077a3
Martin Schrodt <martin@schrodt.org>
2015-10-10 11:28:15 +0200

    adapt to combat log changes in build 1101+

---
+++ src/main/java/skadistats/clarity/model/s1/S1CombatLogEntry.java
@@ -0,0 +1,423 @@
+package skadistats.clarity.model.s1;
+
+import skadistats.clarity.model.CombatLogEntry;
+import skadistats.clarity.model.GameEvent;
+import skadistats.clarity.model.StringTable;
+import skadistats.clarity.wire.common.proto.DotaUserMessages;
+
+public class S1CombatLogEntry implements CombatLogEntry {
+
+    private final S1CombatLogIndices indices;
+    private final StringTable combatLogNames;
+    private final GameEvent e;
+
+    public S1CombatLogEntry(S1CombatLogIndices indices, StringTable combatLogNames, GameEvent event) {
+        this.indices = indices;
+        this.combatLogNames = combatLogNames;
+        this.e = event;
+    }
+
+    private String readCombatLogName(int idx) {
+        return idx == 0 ? null : combatLogNames.getNameByIndex(idx);
+    }
+
+    @Override
+    public boolean hasType() {
+        return true;
+    }
+
+    @Override
+    public DotaUserMessages.DOTA_COMBATLOG_TYPES getType() {
+        return DotaUserMessages.DOTA_COMBATLOG_TYPES.valueOf((int) e.getProperty(indices.typeIdx));
+    }
+
+    @Override
+    public boolean hasTargetName() {
+        return (int) e.getProperty(indices.targetNameIdx) != 0;
+    }
+
+    @Override
+    public String getTargetName() {
+        return readCombatLogName((int) e.getProperty(indices.targetNameIdx));
+    }
+
+    @Override
+    public boolean hasTargetSourceName() {
+        return (int) e.getProperty(indices.targetSourceNameIdx) != 0;
+    }
+
+    @Override
+    public String getTargetSourceName() {
+        return readCombatLogName((int) e.getProperty(indices.targetSourceNameIdx));
+    }
+
+    @Override
+    public boolean hasAttackerName() {
+        return (int) e.getProperty(indices.attackerNameIdx) != 0;
+    }
+
+    @Override
+    public String getAttackerName() {
+        return readCombatLogName((int) e.getProperty(indices.attackerNameIdx));
+    }
+
+    @Override
+    public boolean hasDamageSourceName() {
+        return false;
+    }
+
+    @Override
+    public String getDamageSourceName() {
+        return null;
+    }
+
+    @Override
+    public boolean hasInflictorName() {
+        return (int) e.getProperty(indices.inflictorNameIdx) != 0;
+    }
+
+    @Override
+    public String getInflictorName() {
+        return readCombatLogName((int) e.getProperty(indices.inflictorNameIdx));
+    }
+
+    @Override
+    public boolean hasAttackerIllusion() {
+        return true;
+    }
+
+    @Override
+    public boolean isAttackerIllusion() {
+        return e.getProperty(indices.attackerIllusionIdx);
+    }
+
+    @Override
+    public boolean hasAttackerHero() {
+        return indices.attackerHeroIdx != null;
+    }
+
+    @Override
+    public boolean isAttackerHero() {
+        return e.getProperty(indices.attackerHeroIdx);
+    }
+
+    @Override
+    public boolean hasTargetIllusion() {
+        return true;
+    }
+
+    @Override
+    public boolean isTargetIllusion() {
+        return e.getProperty(indices.targetIllusionIdx);
+    }
+
+    @Override
+    public boolean hasTargetHero() {
+        return indices.targetHeroIdx != null;
+    }
+
+    @Override
+    public boolean isTargetHero() {
+        return e.getProperty(indices.targetHeroIdx);
+    }
+
+    @Override
+    public boolean hasVisibleRadiant() {
+        return false;
+    }
+
+    @Override
+    public boolean isVisibleRadiant() {
+        return false;
+    }
+
+    @Override
+    public boolean hasVisibleDire() {
+        return false;
+    }
+
+    @Override
+    public boolean isVisibleDire() {
+        return false;
+    }
+
+    @Override
+    public boolean hasValue() {
+        return true;
+    }
+
+    @Override
+    public int getValue() {
+        return e.getProperty(indices.valueIdx);
+    }
+
+    @Override
+    public String getValueName() {
+        return readCombatLogName((int)e.getProperty(indices.valueIdx));
+    }
+
+    @Override
+    public boolean hasHealth() {
+        return true;
+    }
+
+    @Override
+    public int getHealth() {
+        return e.getProperty(indices.healthIdx);
+    }
+
+    @Override
+    public boolean hasTimestamp() {
+        return true;
+    }
+
+    @Override
+    public float getTimestamp() {
+        return e.getProperty(indices.timestampIdx);
+    }
+
+    @Override
+    public boolean hasStunDuration() {
+        return false;
+    }
+
+    @Override
+    public float getStunDuration() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasSlowDuration() {
+        return false;
+    }
+
+    @Override
+    public float getSlowDuration() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasAbilityToggleOn() {
+        return indices.abilityToggleOnIdx != null;
+    }
+
+    @Override
+    public boolean isAbilityToggleOn() {
+        return e.getProperty(indices.abilityToggleOnIdx);
+    }
+
+    @Override
+    public boolean hasAbilityToggleOff() {
+        return indices.abilityToggleOffIdx != null;
+    }
+
+    @Override
+    public boolean isAbilityToggleOff() {
+        return e.getProperty(indices.abilityToggleOffIdx);
+    }
+
+    @Override
+    public boolean hasAbilityLevel() {
+        return indices.abilityLevelIdx != null;
+    }
+
+    @Override
+    public int getAbilityLevel() {
+        return e.getProperty(indices.abilityLevelIdx);
+    }
+
+    @Override
+    public boolean hasLocationX() {
+        return false;
+    }
+
+    @Override
+    public float getLocationX() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasLocationY() {
+        return false;
+    }
+
+    @Override
+    public float getLocationY() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasGoldReason() {
+        return indices.goldReasonIdx != null;
+    }
+
+    @Override
+    public int getGoldReason() {
+        return e.getProperty(indices.goldReasonIdx);
+    }
+
+    @Override
+    public boolean hasTimestampRaw() {
+        return indices.timestampRawIdx != null;
+    }
+
+    @Override
+    public float getTimestampRaw() {
+        return e.getProperty(indices.timestampRawIdx);
+    }
+
+    @Override
+    public boolean hasModifierDuration() {
+        return false;
+    }
+
+    @Override
+    public float getModifierDuration() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasXpReason() {
+        return indices.xpReasonIdx != null;
+    }
+
+    @Override
+    public int getXpReason() {
+        return e.getProperty(indices.xpReasonIdx);
+    }
+
+    @Override
+    public boolean hasLastHits() {
+        return false;
+    }
+
+    @Override
+    public int getLastHits() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasAttackerTeam() {
+        return false;
+    }
+
+    @Override
+    public int getAttackerTeam() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasTargetTeam() {
+        return false;
+    }
+
+    @Override
+    public int getTargetTeam() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasObsWardsPlaced() {
+        return false;
+    }
+
+    @Override
+    public int getObsWardsPlaced() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasAssistPlayer0() {
+        return false;
+    }
+
+    @Override
+    public int getAssistPlayer0() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasAssistPlayer1() {
+        return false;
+    }
+
+    @Override
+    public int getAssistPlayer1() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasAssistPlayer2() {
+        return false;
+    }
+
+    @Override
+    public int getAssistPlayer2() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasAssistPlayer3() {
+        return false;
+    }
+
+    @Override
+    public int getAssistPlayer3() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStackCount() {
+        return false;
+    }
+
+    @Override
+    public int getStackCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasHiddenModifier() {
+        return false;
+    }
+
+    @Override
+    public boolean getHiddenModifier() {
+        return false;
+    }
+
+    @Override
+    public boolean hasTargetBuilding() {
+        return false;
+    }
+
+    @Override
+    public boolean isTargetBuilding() {
+        return false;
+    }
+
+    @Override
+    public boolean hasNeutralCampType() {
+        return false;
+    }
+
+    @Override
+    public int getNeutralCampType() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasRuneType() {
+        return false;
+    }
+
+    @Override
+    public int getRuneType() {
+        return 0;
+    }
+
+    public String toString() {
+        return e.toString();
+    }
+}
+
--- src/main/java/skadistats/clarity/processor/gameevents/CombatLog.java
+++ src/main/java/skadistats/clarity/processor/gameevents/CombatLog.java
@@ -2,13 +2,18 @@ package skadistats.clarity.processor.gameevents;
 
 import skadistats.clarity.event.Event;
 import skadistats.clarity.event.Provides;
+import skadistats.clarity.model.CombatLogEntry;
 import skadistats.clarity.model.GameEvent;
 import skadistats.clarity.model.GameEventDescriptor;
-import skadistats.clarity.model.StringTable;
+import skadistats.clarity.model.s1.S1CombatLogEntry;
+import skadistats.clarity.model.s1.S1CombatLogIndices;
+import skadistats.clarity.model.s2.S2CombatLogEntry;
+import skadistats.clarity.processor.reader.OnMessage;
 import skadistats.clarity.processor.reader.OnTickEnd;
 import skadistats.clarity.processor.runner.Context;
 import skadistats.clarity.processor.stringtables.StringTables;
 import skadistats.clarity.processor.stringtables.UsesStringTable;
+import skadistats.clarity.wire.common.proto.DotaUserMessages;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -19,197 +24,40 @@ public class CombatLog {
     public static final String STRING_TABLE_NAME = "CombatLogNames";
     public static final String GAME_EVENT_NAME = "dota_combatlog";
 
-    private final List<Entry> logEntries = new LinkedList<>();
+    private S1CombatLogIndices indices = null;
 
-    private int typeIdx;
-    private int sourceNameIdx;
-    private int targetNameIdx;
-    private int attackerNameIdx;
-    private int inflictorNameIdx;
-    private int attackerIllusionIdx;
-    private int targetIllusionIdx;
-    private int valueIdx;
-    private int healthIdx;
-    private int timestampIdx;
-    private int targetSourceNameIdx;
-    private Integer timestampRawIdx;
-    private Integer attackerHeroIdx;
-    private Integer targetHeroIdx;
-    private Integer abilityToggleOnIdx;
-    private Integer abilityToggleOffIdx;
-    private Integer abilityLevelIdx;
-    private Integer goldReasonIdx;
-    private Integer xpReasonIdx;
-
-    // those indices are in the combat log protobuf, but do never show up in the replay data
-    Integer stunDurationIdx = 16;
-    Integer slowDurationIdx = 17;
-    Integer locationXIdx = 21;
-    Integer locationYIdx = 22;
-    Integer modifierDurationIdx = 25;
-    Integer lastHitsIdx = 27;
-    Integer attackerTeamIdx = 28;
-    Integer targetTeamIdx = 29;
-    Integer obsWardsPlacedIdx = 30;
-    Integer assist_player0Idx = 31;
-    Integer assist_player1Idx = 32;
-    Integer assist_player2Idx = 33;
-    Integer assist_player3Idx = 34;
-    Integer stack_countIdx = 35;
-    Integer hidden_modifierIdx = 36;
+    private final List<CombatLogEntry> logEntries = new LinkedList<>();
 
     @OnGameEventDescriptor(GAME_EVENT_NAME)
-    @UsesStringTable(CombatLog.STRING_TABLE_NAME)
+    @UsesStringTable(STRING_TABLE_NAME)
     public void onGameEventDescriptor(Context ctx, GameEventDescriptor descriptor) {
-        typeIdx = descriptor.getIndexForKey("type");
-        sourceNameIdx = descriptor.getIndexForKey("sourcename");
-        targetNameIdx = descriptor.getIndexForKey("targetname");
-        attackerNameIdx = descriptor.getIndexForKey("attackername");
-        inflictorNameIdx = descriptor.getIndexForKey("inflictorname");
-        attackerIllusionIdx = descriptor.getIndexForKey("attackerillusion");
-        targetIllusionIdx = descriptor.getIndexForKey("targetillusion");
-        valueIdx = descriptor.getIndexForKey("value");
-        healthIdx = descriptor.getIndexForKey("health");
-        timestampIdx = descriptor.getIndexForKey("timestamp");
-        targetSourceNameIdx = descriptor.getIndexForKey("targetsourcename");
-        timestampRawIdx = descriptor.getIndexForKey("timestampraw");
-        attackerHeroIdx = descriptor.getIndexForKey("attackerhero");
-        targetHeroIdx = descriptor.getIndexForKey("targethero");
-        abilityToggleOnIdx = descriptor.getIndexForKey("ability_toggle_on");
-        abilityToggleOffIdx = descriptor.getIndexForKey("ability_toggle_off");
-        abilityLevelIdx = descriptor.getIndexForKey("ability_level");
-        goldReasonIdx = descriptor.getIndexForKey("gold_reason");
-        xpReasonIdx = descriptor.getIndexForKey("xp_reason");
+        indices = new S1CombatLogIndices(descriptor);
     }
 
     @OnGameEvent(GAME_EVENT_NAME)
     public void onGameEvent(Context ctx, GameEvent gameEvent) {
-        logEntries.add(new Entry(ctx, gameEvent));
+        logEntries.add(new S1CombatLogEntry(
+            indices,
+            ctx.getProcessor(StringTables.class).forName(STRING_TABLE_NAME),
+            gameEvent
+        ));
+    }
+
+    @OnMessage(DotaUserMessages.CMsgDOTACombatLogEntry.class)
+    public void onNewCombatLogMessage(Context ctx, DotaUserMessages.CMsgDOTACombatLogEntry message) {
+        logEntries.add(new S2CombatLogEntry(
+            ctx.getProcessor(StringTables.class).forName(STRING_TABLE_NAME),
+            message
+        ));
     }
 
     @OnTickEnd
     public void onTickEnd(Context ctx, boolean synthetic) {
-        Event<OnCombatLogEntry> ev = ctx.createEvent(OnCombatLogEntry.class, Entry.class);
-        for (Entry e : logEntries) {
+        Event<OnCombatLogEntry> ev = ctx.createEvent(OnCombatLogEntry.class, CombatLogEntry.class);
+        for (CombatLogEntry e : logEntries) {
             ev.raise(e);
         }
         logEntries.clear();
     }
 
-    public class Entry {
-
-        private final StringTable combatLogNames;
-        private final GameEvent event;
-
-        private Entry(Context ctx, GameEvent event) {
-            this.combatLogNames = ctx.getProcessor(StringTables.class).forName(STRING_TABLE_NAME);
-            this.event = event;
-        }
-
-        private String readCombatLogName(int idx) {
-            return idx == 0 ? null : combatLogNames.getNameByIndex(idx);
-        }
-
-        public GameEvent getGameEvent() {
-            return event;
-        }
-
-        public int getType() {
-            return event.getProperty(typeIdx);
-        }
-
-        public String getSourceName() {
-            return readCombatLogName((int) event.getProperty(sourceNameIdx));
-        }
-
-        public String getTargetName() {
-            return readCombatLogName((int) event.getProperty(targetNameIdx));
-        }
-
-        public String getAttackerName() {
-            return readCombatLogName((int) event.getProperty(attackerNameIdx));
-        }
-
-        public String getInflictorName() {
-            return readCombatLogName((int) event.getProperty(inflictorNameIdx));
-        }
-
-        public boolean isAttackerIllusion() {
-            return event.getProperty(attackerIllusionIdx);
-        }
-
-        public boolean isTargetIllusion() {
-            return event.getProperty(targetIllusionIdx);
-        }
-
-        public int getValue() {
-            return event.getProperty(valueIdx);
-        }
-
-        public int getHealth() {
-            return event.getProperty(healthIdx);
-        }
-
-        public float getTimestamp() {
-            return event.getProperty(timestampIdx);
-        }
-
-        public String getTargetSourceName() {
-            return readCombatLogName((int) event.getProperty(targetSourceNameIdx));
-        }
-
-        public float getTimestampRaw() {
-            return event.getProperty(timestampRawIdx);
-        }
-
-        public boolean isAbilityToggleOn() {
-            return event.getProperty(abilityToggleOnIdx);
-        }
-
-        public boolean isAbilityToggleOff() {
-            return event.getProperty(abilityToggleOffIdx);
-        }
-
-        public int getAbilityLevel() {
-            return event.getProperty(abilityLevelIdx);
-        }
-
-        //TODO: for full safety, all getters should check for null on Idx
-        public String toString() {
-            //print the underlying gameevent
-            return event.toString();
-        }
-
-        public boolean isTargetHero() {
-            if (targetHeroIdx == null) {
-                return true;
-            }
-            return event.getProperty(targetHeroIdx);
-        }
-
-        public boolean isAttackerHero() {
-            if (attackerHeroIdx == null) {
-                return true;
-            }
-            return event.getProperty(attackerHeroIdx);
-        }
-
-        public int getGoldReason() {
-            if (goldReasonIdx == null) {
-                return 0;
-            }
-            return event.getProperty(goldReasonIdx);
-        }
-
-        public int getXpReason() {
-            if (xpReasonIdx == null) {
-                return 0;
-            }
-            return event.getProperty(xpReasonIdx);
-        }
-
-        public String getValueName() {
-            return readCombatLogName(getValue());
-        }
-    }
 }
