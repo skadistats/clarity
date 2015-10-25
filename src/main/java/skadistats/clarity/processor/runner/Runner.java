218efe5d89c1b562da349174cd0c9b95ed2be799
Martin Schrodt <martin@schrodt.org>
2015-10-25 13:28:43 +0100

    add getSource() to Runner interface

--- src/main/java/skadistats/clarity/processor/runner/Runner.java
+++ src/main/java/skadistats/clarity/processor/runner/Runner.java
@@ -1,11 +1,13 @@
 package skadistats.clarity.processor.runner;
 
 import skadistats.clarity.model.EngineType;
+import skadistats.clarity.source.Source;
 
 public interface Runner<T extends Runner> {
 
     Context getContext();
     int getTick();
+    Source getSource();
     EngineType getEngineType();
 
     T runWith(Object... processors);
