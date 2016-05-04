f40adfdac7a5ee9bca081f7fd2c9632b4b8372a0
Robert Smieja <robertsmieja@gmail.com>
2016-05-04 00:46:34 -0400

    Work on SourceTest
    -Reformat code
    -Remove tests of abstract method stubs
    -Partial coverage for readVarInt32

--- src/test/java/skadistats/clarity/source/SourceTest.java
+++ src/test/java/skadistats/clarity/source/SourceTest.java
@@ -1,228 +1,233 @@
 package skadistats.clarity.source;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
 public class SourceTest {
-	//Source is Abstract so we need an implementing class
-	public class SourceDummyClass extends Source {
-		@Override
-		public int getPosition() {return 0;}
+    //Source is Abstract so we need an implementing class
+    public class SourceDummyClass extends Source {
+        @Override
+        public int getPosition() {
+            return 0;
+        }
 
-		@Override
-		public void setPosition(int position) throws IOException {}
+        @Override
+        public void setPosition(int position) throws IOException {
+        }
 
-		@Override
-		public byte readByte() throws IOException {return 0;}
+        @Override
+        public byte readByte() throws IOException {
+            return 0;
+        }
 
-		@Override
-		public void readBytes(byte[] dest, int offset, int length) throws IOException {}
-	}
+        @Override
+        public void readBytes(byte[] dest, int offset, int length) throws IOException {
+        }
+    }
 
-	Source source;
+    Source source;
 
-	@Before
-	public void setUp() throws Exception {
-		source = new SourceDummyClass();
-	}
+    @Before
+    public void setUp() throws Exception {
+        source = new SourceDummyClass();
+    }
 
-	@Test
-	public void testReadBytesByteArrayIntInt() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testReadBytesInt() throws IOException {
+        Source sourceSpy = spy(source);
 
-	@Test
-	public void testReadBytesInt() throws IOException {
-		source = spy(source);
+        Answer<Void> readBytesAnswer = new Answer<Void>() {
+            public Void answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                byte[] dst = (byte[]) args[0];
 
-		Answer<Void> readBytesAnswer = new Answer<Void>(){
-			public Void answer(InvocationOnMock invocation) {
-				Object[] args = invocation.getArguments();
-				byte[] dst = (byte[]) args[0];
+                for (int i = 0; i < dst.length; i++) {
+                    dst[i] = (byte) i;
+                }
 
-				for(int i = 0; i < dst.length; i++){
-					dst[i] = (byte)i;
-				}
+                return null;
+            }
+        };
+        doAnswer(readBytesAnswer).when(sourceSpy).readBytes(any(byte[].class), eq(0), eq(5));
 
-				return null;
-			}
-		};
-		doAnswer(readBytesAnswer).when(source).readBytes(any(byte[].class), eq(0), eq(5));
+        byte[] expected = new byte[]{0x0, 0x1, 0x2, 0x3, 0x4};
+        byte[] actual = sourceSpy.readBytes(5);
 
-		byte[] expected = new byte[]{0x0, 0x1, 0x2, 0x3, 0x4};
-		byte[] actual = source.readBytes(5);
+        assertArrayEquals(expected, actual);
+    }
 
-		assertArrayEquals(expected, actual);
-	}
+    @Test
+    public void readFixedInt32_bigEndian() throws IOException {
+        Source sourceSpy = spy(source);
 
-	@Test
-	public void readFixedInt32_bigEndian() throws IOException {
-		source = spy(source);
+        ByteBuffer returnValue = ByteBuffer.allocate(4).putInt(Integer.MAX_VALUE);
+        returnValue = returnValue.order(ByteOrder.LITTLE_ENDIAN);
+        returnValue.flip();
 
-		ByteBuffer returnValue = ByteBuffer.allocate(4).putInt(Integer.MAX_VALUE);
-		returnValue = returnValue.order(ByteOrder.LITTLE_ENDIAN);
-		returnValue.flip();
-		
-		ByteBuffer expected = ByteBuffer.allocate(4).putInt(Integer.MAX_VALUE);
-		expected = expected.order(ByteOrder.LITTLE_ENDIAN);
-		expected.flip();
-		
-		when(source.readBytes(4)).thenReturn(returnValue.array());
-		
-		int actual = source.readFixedInt32();
-		
-		assertEquals(expected.getInt(), actual);;
-	}
+        ByteBuffer expected = ByteBuffer.allocate(4).putInt(Integer.MAX_VALUE);
+        expected = expected.order(ByteOrder.LITTLE_ENDIAN);
+        expected.flip();
 
-	@Test
-	public void readFixedInt32_litleEndian() throws IOException {
-		source = spy(source);
+        when(sourceSpy.readBytes(4)).thenReturn(returnValue.array());
 
-		ByteBuffer returnValue = ByteBuffer.allocate(4).putInt(Integer.MAX_VALUE);
-		returnValue = returnValue.order(ByteOrder.BIG_ENDIAN);
-		returnValue.flip();
+        int actual = sourceSpy.readFixedInt32();
 
-		ByteBuffer expected = ByteBuffer.allocate(4).putInt(Integer.MAX_VALUE);
-		expected = expected.order(ByteOrder.LITTLE_ENDIAN);
-		expected.flip();
-		
-		when(source.readBytes(4)).thenReturn(expected.array());
-		
-		int actual = source.readFixedInt32();
-		
-		assertEquals(expected.getInt(), actual);
-	}
+        assertEquals(expected.getInt(), actual);
+        ;
+    }
 
-	@Test
-	public void testReadVarInt32() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void readFixedInt32_litleEndian() throws IOException {
+        Source sourceSpy = spy(source);
 
-	@Test
-	public void testReadFixedInt32() {
-		fail("Not yet implemented");
-	}
+        ByteBuffer returnValue = ByteBuffer.allocate(4).putInt(Integer.MAX_VALUE);
+        returnValue = returnValue.order(ByteOrder.BIG_ENDIAN);
+        returnValue.flip();
 
-	@Test
-	public void testSkipBytes() throws IOException {
-		source = spy(source);
-		when(source.getPosition()).thenReturn(42);
+        ByteBuffer expected = ByteBuffer.allocate(4).putInt(Integer.MAX_VALUE);
+        expected = expected.order(ByteOrder.LITTLE_ENDIAN);
+        expected.flip();
 
-		source.skipBytes(4);
-		
-		verify(source, times(1)).setPosition(46);
-	}
+        when(sourceSpy.readBytes(4)).thenReturn(expected.array());
 
-	@Test
-	public void testSkipVarInt32_notEmpty() throws IOException {
-		source = spy(source);
-		when(source.readByte()).thenReturn((byte)1);
-		
-		source.skipVarInt32();
-		
-		verify(source, times(1)).readByte();
-	}
-	
-	@Test
-	public void testSkipVarInt32_firstByteEmpty() throws IOException {
-		source = spy(source);
-		when(source.readByte())
-		.thenReturn((byte)-1)
-		.thenReturn((byte)1);
-		
-		source.skipVarInt32();
-		
-		verify(source, times(2)).readByte();
-	}
-	
-	@Test
-	public void testSkipVarInt32_secondByteEmpty() throws IOException {
-		source = spy(source);
-		when(source.readByte())
-		.thenReturn((byte)-1)
-		.thenReturn((byte)-1)
-		.thenReturn((byte)1);
-		
-		source.skipVarInt32();
-		
-		verify(source, times(3)).readByte();
-	}
-	
-	@Test
-	public void testSkipVarInt32_thirdByteEmpty() throws IOException {
-		source = spy(source);
-		when(source.readByte())
-		.thenReturn((byte)-1)
-		.thenReturn((byte)-1)
-		.thenReturn((byte)-1)
-		.thenReturn((byte)1);
-		
-		source.skipVarInt32();
-		
-		verify(source, times(4)).readByte();
-	}
-	
-	@Test
-	public void testSkipVarInt32_fourthByteEmpty() throws IOException {
-		source = spy(source);
-		when(source.readByte())
-		.thenReturn((byte)-1)
-		.thenReturn((byte)-1)
-		.thenReturn((byte)-1)
-		.thenReturn((byte)-1)
-		.thenReturn((byte)1);
-		
-		source.skipVarInt32();
-		
-		verify(source, times(5)).readByte();
-	}
+        int actual = sourceSpy.readFixedInt32();
 
-	@Test
-	public void testReadEngineType() {
-		fail("Not yet implemented");
-	}
+        assertEquals(expected.getInt(), actual);
+    }
 
-	@Test
-	public void testGetFullPacketsBeforeTick() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testReadVarInt32_positiveInt() throws IOException {
+        int expectedResult = 127;
 
-	@Test
-	public void testGetLastTick() {
-		fail("Not yet implemented");
-	}
+        Source sourceSpy = spy(source);
+        doReturn((byte) expectedResult).when(sourceSpy).readByte();
 
-	//Stubs of Abstract methods
-	@Test
-	public void testGetPosition() {
-		assertEquals(0, source.getPosition());
-	}
+        int result = sourceSpy.readVarInt32();
 
-	@Test
-	public void testSetPosition() throws IOException {
-		//Nothing to assert
-		source.setPosition(99);
-	}
+        assertEquals(expectedResult, result);
+    }
 
-	@Test
-	public void testReadByte() throws IOException {
-		assertEquals(0, source.readByte());
-	}
+
+    @Test
+    public void testReadVarInt32_negativeInt() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testReadVarInt32_malformedInt() throws IOException {
+        Source sourceSpy = spy(source);
+        doReturn((byte) 0xFF).when(sourceSpy).readByte();
+
+        try {
+            int result = sourceSpy.readVarInt32();
+            fail("This should have failed with an exception");
+        } catch (IOException exception) {
+            assertEquals("malformed varint detected", exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testReadFixedInt32() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSkipBytes() throws IOException {
+        Source sourceSpy = spy(source);
+        when(sourceSpy.getPosition()).thenReturn(42);
+
+        sourceSpy.skipBytes(4);
+
+        verify(sourceSpy, times(1)).setPosition(46);
+    }
+
+    @Test
+    public void testSkipVarInt32_notEmpty() throws IOException {
+        Source sourceSpy = spy(source);
+        when(sourceSpy.readByte()).thenReturn((byte) 1);
+
+        sourceSpy.skipVarInt32();
+
+        verify(sourceSpy, times(1)).readByte();
+    }
+
+    @Test
+    public void testSkipVarInt32_firstByteEmpty() throws IOException {
+        Source sourceSpy = spy(source);
+        when(sourceSpy.readByte())
+                .thenReturn((byte) -1)
+                .thenReturn((byte) 1);
+
+        sourceSpy.skipVarInt32();
+
+        verify(sourceSpy, times(2)).readByte();
+    }
+
+    @Test
+    public void testSkipVarInt32_secondByteEmpty() throws IOException {
+        Source sourceSpy = spy(source);
+        when(sourceSpy.readByte())
+                .thenReturn((byte) -1)
+                .thenReturn((byte) -1)
+                .thenReturn((byte) 1);
+
+        sourceSpy.skipVarInt32();
+
+        verify(sourceSpy, times(3)).readByte();
+    }
+
+    @Test
+    public void testSkipVarInt32_thirdByteEmpty() throws IOException {
+        Source sourceSpy = spy(source);
+        when(sourceSpy.readByte())
+                .thenReturn((byte) -1)
+                .thenReturn((byte) -1)
+                .thenReturn((byte) -1)
+                .thenReturn((byte) 1);
+
+        sourceSpy.skipVarInt32();
+
+        verify(sourceSpy, times(4)).readByte();
+    }
+
+    @Test
+    public void testSkipVarInt32_fourthByteEmpty() throws IOException {
+        Source sourceSpy = spy(source);
+        when(sourceSpy.readByte())
+                .thenReturn((byte) -1)
+                .thenReturn((byte) -1)
+                .thenReturn((byte) -1)
+                .thenReturn((byte) -1)
+                .thenReturn((byte) 1);
+
+        sourceSpy.skipVarInt32();
+
+        verify(sourceSpy, times(5)).readByte();
+    }
+
+    @Test
+    public void testReadEngineType() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetFullPacketsBeforeTick() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetLastTick() {
+        fail("Not yet implemented");
+    }
 }
