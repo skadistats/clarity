6f9c3c74483c006f6c41db458dfb5189a59957eb
Martin Schrodt <clarity@martin.schrodt.org>
2016-09-18 16:39:08 +0200

    port modifiers processor to new injection model

--- src/main/java/skadistats/clarity/processor/modifiers/Modifiers.java
+++ src/main/java/skadistats/clarity/processor/modifiers/Modifiers.java
@@ -2,20 +2,24 @@ package skadistats.clarity.processor.modifiers;
 
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
+import skadistats.clarity.event.Event;
+import skadistats.clarity.event.InsertEvent;
 import skadistats.clarity.event.Provides;
 import skadistats.clarity.model.StringTable;
-import skadistats.clarity.processor.runner.Context;
 import skadistats.clarity.processor.stringtables.OnStringTableEntry;
 import skadistats.clarity.wire.common.proto.DotaModifiers;
 
 @Provides({OnModifierTableEntry.class})
 public class Modifiers {
 
+    @InsertEvent
+    private Event<OnModifierTableEntry> evEntry;
+
     @OnStringTableEntry("ActiveModifiers")
-    public void onTableEntry(Context ctx, StringTable table, int index, String key, ByteString value) throws InvalidProtocolBufferException {
+    public void onTableEntry(StringTable table, int index, String key, ByteString value) throws InvalidProtocolBufferException {
         if (value != null) {
             DotaModifiers.CDOTAModifierBuffTableEntry message = DotaModifiers.CDOTAModifierBuffTableEntry.parseFrom(value);
-            ctx.createEvent(OnModifierTableEntry.class, DotaModifiers.CDOTAModifierBuffTableEntry.class).raise(message);
+            evEntry.raise(message);
         }
     }
 
