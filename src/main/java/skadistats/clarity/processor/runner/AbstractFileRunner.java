f3b9930e1f4005b6539ba36b2c18ab58f207682d
Martin Schrodt <clarity@martin.schrodt.org>
2016-10-08 13:34:41 +0200

    move replay tick handling into AbstractFileRunner

--- src/main/java/skadistats/clarity/processor/runner/AbstractFileRunner.java
+++ src/main/java/skadistats/clarity/processor/runner/AbstractFileRunner.java
@@ -1,26 +1,67 @@
 package skadistats.clarity.processor.runner;
 
+import skadistats.clarity.event.Event;
+import skadistats.clarity.event.InsertEvent;
 import skadistats.clarity.event.Provides;
 import skadistats.clarity.model.EngineType;
+import skadistats.clarity.processor.reader.OnTickEnd;
+import skadistats.clarity.processor.reader.OnTickStart;
 import skadistats.clarity.source.Source;
 
 import java.io.IOException;
 
-@Provides({OnInputSource.class})
+@Provides(value = {OnInputSource.class, OnTickStart.class, OnTickEnd.class}, runnerClass = { AbstractFileRunner.class })
 public abstract class AbstractFileRunner extends AbstractRunner implements FileRunner {
+
+    @InsertEvent
+    private Event<OnTickStart> evTickStart;
+    @InsertEvent
+    private Event<OnTickEnd> evTickEnd;
 
     protected final Source source;
     protected LoopController loopController;
 
+    /* tick the user is at the end of */
+    protected int tick;
+    /* tick is synthetic (does not contain replay data) */
+    protected boolean synthetic = true;
+
     public AbstractFileRunner(Source source, EngineType engineType) throws IOException {
         super(engineType);
         this.source = source;
+        this.tick = -1;
         engineType.skipHeaderOffsets(source);
     }
 
     protected void initAndRunWith(Object... processors) {
         initWithProcessors(processors);
         context.createEvent(OnInputSource.class, Source.class, LoopController.class).raise(source, loopController);
+    }
+
+    protected void endTicksUntil(int untilTick) {
+        while (tick < untilTick) {
+            evTickEnd.raise(synthetic);
+            setTick(tick + 1);
+            synthetic = true;
+            evTickStart.raise(synthetic);
+        }
+        evTickEnd.raise(synthetic);
+        synthetic = false;
+    }
+
+    protected void startNewTick(int upcomingTick) {
+        setTick(tick + 1);
+        synthetic = tick != upcomingTick;
+        evTickStart.raise(synthetic);
+    }
+
+    protected void setTick(int tick) {
+        this.tick = tick;
+    }
+
+    @Override
+    public int getTick() {
+        return tick;
     }
 
     @Override
--- src/main/java/skadistats/clarity/processor/runner/AbstractRunner.java
+++ src/main/java/skadistats/clarity/processor/runner/AbstractRunner.java
@@ -6,32 +6,20 @@ import skadistats.clarity.event.Event;
 import skadistats.clarity.event.InsertEvent;
 import skadistats.clarity.event.Provides;
 import skadistats.clarity.model.EngineType;
-import skadistats.clarity.processor.reader.OnTickEnd;
-import skadistats.clarity.processor.reader.OnTickStart;
 
-@Provides({OnTickStart.class, OnTickEnd.class, OnInit.class})
+@Provides({OnInit.class})
 public abstract class AbstractRunner implements Runner {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
-    @InsertEvent
-    private Event<OnTickStart> evTickStart;
-    @InsertEvent
-    private Event<OnTickEnd> evTickEnd;
     @InsertEvent
     private Event<OnInit> evInitRun;
 
     protected final EngineType engineType;
     protected Context context;
 
-    /* tick the user is at the end of */
-    protected int tick;
-    /* tick is synthetic (does not contain replay data) */
-    protected boolean synthetic = true;
-
     public AbstractRunner(EngineType engineType) {
         this.engineType = engineType;
-        this.tick = -1;
     }
 
     private ExecutionModel createExecutionModel(Object... processors) {
@@ -49,32 +37,6 @@ public abstract class AbstractRunner implements Runner {
         if (evInitRun != null) {
             evInitRun.raise();
         }
-    }
-
-    protected void endTicksUntil(int untilTick) {
-        while (tick < untilTick) {
-            evTickEnd.raise(synthetic);
-            setTick(tick + 1);
-            synthetic = true;
-            evTickStart.raise(synthetic);
-        }
-        evTickEnd.raise(synthetic);
-        synthetic = false;
-    }
-
-    protected void startNewTick(int upcomingTick) {
-        setTick(tick + 1);
-        synthetic = tick != upcomingTick;
-        evTickStart.raise(synthetic);
-    }
-
-    protected void setTick(int tick) {
-        this.tick = tick;
-    }
-
-    @Override
-    public int getTick() {
-        return tick;
     }
 
     @Override
