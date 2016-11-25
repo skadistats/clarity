499ac322e8d30e05ca94bf1ce481cba88c23be59
Martin Schrodt <clarity@martin.schrodt.org>
2016-11-25 15:45:33 +0100

    prefix logging channels with "clarity."

--- src/main/java/skadistats/clarity/LogChannel.java
+++ src/main/java/skadistats/clarity/LogChannel.java
@@ -1,8 +1,8 @@
 package skadistats.clarity;
 
-public enum LogChannel {
-    executionModel,
-    runner,
-    unpacker,
-    entities
+public class LogChannel {
+    public static final String executionModel = "clarity.execution";
+    public static final String runner = "clarity.runner";
+    public static final String unpacker = "clarity.unpacker";
+    public static final String entities = "clarity.entities";
 }
