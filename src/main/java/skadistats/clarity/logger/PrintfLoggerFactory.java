c32a9be3bb099fd9c96165ec9f400d7bece39875
Martin Schrodt <clarity@martin.schrodt.org>
2016-11-29 19:37:35 +0100

    remove PrintfLogger from logging stacktrace

--- src/main/java/skadistats/clarity/logger/PrintfLoggerFactory.java
+++ src/main/java/skadistats/clarity/logger/PrintfLoggerFactory.java
@@ -1,11 +1,26 @@
 package skadistats.clarity.logger;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PrintfLoggerFactory {
 
     private static final Map<String, PrintfLogger> loggerCache = new HashMap<>();
+
+    static {
+        try {
+            ILoggerFactory f = LoggerFactory.getILoggerFactory();
+            Class<? extends ILoggerFactory> c = f.getClass();
+            if (c.getName().equals("ch.qos.logback.classic.LoggerContext")) {
+                List<String> frameworkPackages = (List<String>) c.getMethod("getFrameworkPackages").invoke(f);
+                frameworkPackages.add("skadistats.clarity.logger");
+            }
+        } catch (Exception e) {}
+    }
 
     public static PrintfLogger getLogger(Enum category) {
         return getLogger(category.name());
