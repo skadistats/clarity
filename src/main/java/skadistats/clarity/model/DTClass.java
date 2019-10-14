40cd060c7f5884813e08b7110fcef2bdf0a5fd2a
Martin Schrodt <clarity@martin.schrodt.org>
2019-10-14 15:06:27 +0200

    fix collectFieldPaths() missing

--- src/main/java/skadistats/clarity/model/DTClass.java
+++ src/main/java/skadistats/clarity/model/DTClass.java
@@ -2,6 +2,10 @@ package skadistats.clarity.model;
 
 import skadistats.clarity.model.state.EntityState;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public interface DTClass {
 
     String getDtName();
@@ -13,6 +17,16 @@ public interface DTClass {
 
     String getNameForFieldPath(FieldPath fp);
     FieldPath getFieldPathForName(String property);
+
+    @Deprecated
+    default List<FieldPath> collectFieldPaths(EntityState state) {
+        List<FieldPath> result = new ArrayList<>();
+        Iterator<FieldPath> iter = state.fieldPathIterator();
+        while (iter.hasNext()) {
+            result.add(iter.next());
+        }
+        return result;
+    }
 
 }
 
