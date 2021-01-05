fa6fdd368b659771911031917bc5f0801ab62172
Martin Schrodt <martin@schrodt.org>
2021-01-06 00:35:37 +0100

    add utility function to evaluate something depending on whether source 1 or source 2

--- src/main/java/skadistats/clarity/model/DTClass.java
+++ src/main/java/skadistats/clarity/model/DTClass.java
@@ -1,10 +1,13 @@
 package skadistats.clarity.model;
 
+import skadistats.clarity.io.s1.S1DTClass;
+import skadistats.clarity.io.s2.S2DTClass;
 import skadistats.clarity.model.state.EntityState;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Function;
 
 public interface DTClass {
 
@@ -26,6 +29,14 @@ public interface DTClass {
             result.add(iter.next());
         }
         return result;
+    }
+
+    default <V> V evaluate(Function<S1DTClass, V> s1, Function<S2DTClass, V> s2) {
+        if (this instanceof S2DTClass) {
+            return s2.apply((S2DTClass) this);
+        } else {
+            return s1.apply((S1DTClass) this);
+        }
     }
 
 }
