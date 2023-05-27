421af4ffe64b668d21fd00f0823f2cff1a23e545
Martin Schrodt <martin@schrodt.org>
2023-05-27 18:35:36 +0300

    feature: add basic PlayerInfo-parsing for CSGO 2

--- src/main/java/skadistats/clarity/model/csgo/PlayerInfoType.java
+++ src/main/java/skadistats/clarity/model/csgo/PlayerInfoType.java
@@ -3,6 +3,8 @@ package skadistats.clarity.model.csgo;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.ZeroCopy;
 import skadistats.clarity.io.Util;
+import skadistats.clarity.wire.Packet;
+import skadistats.clarity.wire.csgo.s2.proto.CSGOS2ClarityMessages;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -26,27 +28,57 @@ public class PlayerInfoType {
     private final boolean isHltv;
     private final int[] customFiles;
 
-    public PlayerInfoType(ByteString byteString) throws IOException {
+    public static PlayerInfoType createS1(ByteString byteString) throws IOException {
         ByteBuffer buf = ByteBuffer.wrap(ZeroCopy.extract(byteString)).order(ByteOrder.BIG_ENDIAN);
-        version = buf.getLong();
-        xuid = buf.getLong();
-        name = readZeroTerminated(buf, MAX_PLAYER_NAME_LENGTH);
-        userId = buf.getInt();
-        guid = readZeroTerminated(buf, SIGNED_GUID_LEN + 1);
-        friendsId = buf.getInt();
+        long version = buf.getLong();
+        long xuid = buf.getLong();
+        String name = readZeroTerminated(buf, MAX_PLAYER_NAME_LENGTH);
+        int userId = buf.getInt();
+        String guid = readZeroTerminated(buf, SIGNED_GUID_LEN + 1);
+        int friendsId = buf.getInt();
         buf.position(buf.position() + 3);
-        friendsName = readZeroTerminated(buf, MAX_PLAYER_NAME_LENGTH);
-        fakePlayer = buf.get() == (byte)1;
-        isHltv = buf.get() == (byte)1;
-        customFiles = new int[MAX_CUSTOM_FILES];
+        String friendsName = readZeroTerminated(buf, MAX_PLAYER_NAME_LENGTH);
+        boolean fakePlayer = buf.get() == (byte)1;
+        boolean isHltv = buf.get() == (byte)1;
+        int[] customFiles = new int[MAX_CUSTOM_FILES];
         for (int i = 0; i < MAX_CUSTOM_FILES; i++) {
             customFiles[i] = buf.getInt();
         }
         // TODO: still 6 bytes remaining, so something is wrong...
+        return new PlayerInfoType(version, xuid, name, userId, guid, friendsId, friendsName, fakePlayer, isHltv, customFiles);
     }
 
-    private String readZeroTerminated(ByteBuffer buffer, int size) throws IOException {
+    public static PlayerInfoType createS2(ByteString byteString) {
+        CSGOS2ClarityMessages.PlayerInfo msg = Packet.parse(CSGOS2ClarityMessages.PlayerInfo.class, byteString);
+        return new PlayerInfoType(
+                -1L,
+                msg.getXuid(),
+                msg.getName(),
+                -1,
+                "",
+                -1,
+                "",
+                msg.getFakePlayer(),
+                msg.getIsHlTv(),
+                new int[MAX_CUSTOM_FILES]
+        );
+    }
+
+    private static String readZeroTerminated(ByteBuffer buffer, int size) throws IOException {
         return Util.readFixedZeroTerminated(buffer, size);
+    }
+
+    private PlayerInfoType(long version, long xuid, String name, int userId, String guid, int friendsId, String friendsName, boolean fakePlayer, boolean isHltv, int[] customFiles) {
+        this.version = version;
+        this.xuid = xuid;
+        this.name = name;
+        this.userId = userId;
+        this.guid = guid;
+        this.friendsId = friendsId;
+        this.friendsName = friendsName;
+        this.fakePlayer = fakePlayer;
+        this.isHltv = isHltv;
+        this.customFiles = customFiles;
     }
 
     public long getVersion() {
--- src/main/java/skadistats/clarity/processor/stringtables/PlayerInfo.java
+++ src/main/java/skadistats/clarity/processor/stringtables/PlayerInfo.java
@@ -2,9 +2,11 @@ package skadistats.clarity.processor.stringtables;
 
 import com.google.protobuf.ByteString;
 import skadistats.clarity.event.Event;
+import skadistats.clarity.event.Insert;
 import skadistats.clarity.event.InsertEvent;
 import skadistats.clarity.event.Provides;
 import skadistats.clarity.model.EngineId;
+import skadistats.clarity.model.EngineType;
 import skadistats.clarity.model.StringTable;
 import skadistats.clarity.model.csgo.PlayerInfoType;
 
@@ -12,9 +14,12 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
-@Provides(value = {UsesPlayerInfo.class, OnPlayerInfo.class}, engine = { EngineId.CSGO_S1})
+@Provides(value = {UsesPlayerInfo.class, OnPlayerInfo.class}, engine = { EngineId.CSGO_S1, EngineId.CSGO_S2})
 @UsesStringTable("userinfo")
 public class PlayerInfo {
+
+    @Insert
+    private EngineType engineType;
 
     @InsertEvent
     private Event<OnPlayerInfo> evPlayerInfo;
@@ -25,14 +30,23 @@ public class PlayerInfo {
     public void onEntry(StringTable table, int index, String key, ByteString value) throws IOException {
         PlayerInfoType current = null;
         if (value != null && value.size() != 0) {
-            current = new PlayerInfoType(value);
+            switch (engineType.getId()) {
+                case CSGO_S1:
+                    current = PlayerInfoType.createS1(value);
+                    break;
+                case CSGO_S2:
+                    current = PlayerInfoType.createS2(value);
+                    break;
+                default:
+                    throw new UnsupportedOperationException("don't know how to handle player info");
+            }
         }
 
         int i = index + 1;
         PlayerInfoType last = playerInfos.get(i);
 
         if (current == last) return;
-        if (current != null && last != null && current.equals(last)) return;
+        if (current != null && current.equals(last)) return;
 
         if (current == null) {
             playerInfos.remove(i);
