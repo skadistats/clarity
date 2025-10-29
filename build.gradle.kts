b023d906ae4512bb11a3406446f1d0be3946236f
Martin Schrodt <martin@schrodt.org>
2025-10-29 18:11:21 +0200

    fix: publishing to OSSRH staging

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,8 +66,11 @@ publishing {
 }
 
 nexusPublishing {
-    this.repositories {
-        sonatype()
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
     }
 }
 
